09c76f0a2584301fcabcd78cf96595ba53901d2d
bukharyi <bukharyi@gmail.com>
2025-08-24 19:25:08 +0800

    first commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,16 @@
+//helloworld add test. 
+//helloworld add test 2
+//helloworld add test 3
+//helloworld add test 4
+//helloworld add test 5
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getGreeting());
+    }
+
+    public static String getGreeting() {
+        return "Hello, World!";
+    }
+}
